d0607654cbf9ff8e67715d73a368934519c196ee
moisecode <moise.mahara-moukene.2@etsmtl.net>
2021-07-04 00:21:30 -0400

    code simplifie

--- src/main.py
+++ src/main.py
@@ -93,21 +93,21 @@ if __name__ == '__main__':
     
     if "TOKEN" in os.environ:
         token = os.environ["TOKEN"]
-    testtype = 2
-    while testtype not in [0, 1]:
-         testtype = int(input ("Pour un test preconfiguré, entrer le '0', si non le '1' pour choisir limite froid et chaud : "))
+    # testtype = 2
+    # while testtype not in [0, 1]:
+    #      testtype = int(input ("Pour un test preconfiguré, entrer le '0', si non le '1' pour choisir limite froid et chaud : "))
     
     # test automatique avec les valeurs 20 et 80, pour les temperatures
-    if(testtype==0):
-        if "LIMITCHAUD" in os.environ:
-            limitChaud = float(os.environ["LIMITCHAUD"])
-        if "LIMITFROID" in os.environ:
+    # if(testtype==0):
+    if "LIMITCHAUD" in os.environ:
+        limitChaud = float(os.environ["LIMITCHAUD"])
+    if "LIMITFROID" in os.environ:
             limitFroid = float(os.environ["LIMITFROID"])
     # test dynamique. Les valeurs de température dependent de ce que l'utilisateur va entrer
-    elif(testtype==1):
-        limitChaud= int(input("Entrer limite chaleur max de control : "))
-        limitFroid= int(input("Entrer limite froid min de control : "))
-    # test main sans image
+    # elif(testtype==1):
+    #     limitChaud= int(input("Entrer limite chaleur max de control : "))
+    #     limitFroid= int(input("Entrer limite froid min de control : "))
+    # # test main sans image
     
     # exécution de l'application
     main = Main(token, nbTick, limitFroid, limitChaud)
