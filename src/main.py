1582392da45455e34f64bdb5c0ed07534e4f2726
moisecode <moise.mahara-moukene.2@etsmtl.net>
2021-07-07 14:22:26 -0400

    Ajout code pour Mysql

--- src/main.py
+++ src/main.py
@@ -3,9 +3,16 @@ import logging
 import sys
 import requests
 import json
-import time
+import time 
 import os
 import mysql.connector as mysql
+#------------
+from datetime import datetime, date, tzinfo
+import datetime
+
+
+
+
 
 class Main:
     def __init__(self, mytoken, nbTick, limitFroid, limitChaud):
@@ -21,7 +28,17 @@ class Main:
         self.LIMITCHAUD = limitChaud
         self.LIMITFROID = limitFroid
         self.TOKEN = mytoken
-    
+        self.mydb = mysql.connect(
+            user ='tp3',
+            password='Tp@3!55.',
+            host ='ec2-3-237-178-114.compute-1.amazonaws.com',
+            database ='tp3'
+        )
+        if self.mydb.cursor:
+            print("Connection etablie avec la DB")
+        else :
+            print("connection failed")
+        
     def __del__(self):
         if (self._hub_connection != None):
             self._hub_connection.stop()
@@ -51,22 +68,25 @@ class Main:
             "reconnect_interval": 5,
             "max_attempts": 999
         }).build()
-
+        
         self._hub_connection.on("ReceiveSensorData", self.onSensorDataReceived)
         self._hub_connection.on_open(lambda: print("||| Connection opened."))
         self._hub_connection.on_close(lambda: print("||| Connection closed."))
         self._hub_connection.on_error(lambda data: print(f"||| An exception was thrown closed: {data.error}"))
-
+#-------------------------------
     def onSensorDataReceived(self, data):
         try:        
             print(data[0]["date"]  + " --> " + data[0]["data"])
             date = data[0]["date"]
             dp = float(data[0]["data"])
+           
+            #- envois des données
+            self.sendDataToMysql(date,dp)
 
             self.analyzeDatapoint(date, dp)
         except Exception as err:
             print(err)
-    
+
     def analyzeDatapoint(self, date, data):
         if (data >= self.LIMITCHAUD):                
             self.sendActionToHvac(date, "TurnOnAc", self.NBTICK)
@@ -77,13 +97,49 @@ class Main:
         
         r = requests.get(f"https://log680.vincentboivin.ca/api/hvac/{self.TOKEN}/{action}/{nbTick}") 
         details = json.loads(r.text)
+
+        # envoi des evenements dans mysql
         print(details)
+        self.sendEventsToMysql(date, str(details))
+
+
+        
+    # send temperature data  to mysql
+    def sendDataToMysql(self,date, temp):
+       
+        query = "INSERT INTO log680_tp3(heure,temperature) " \
+            "VALUES(%s,%s)"
+        args = (date, temp)
+
+        try:
+            cursor = self.mydb.cursor()
+            cursor.execute(query, args)
+            self.mydb.commit()
+        except Error as error:
+            print(error)
+
+    # send events data  to mysql
+    def sendEventsToMysql(self,date, ev):
+       
+        query = "INSERT INTO log680_EVENS(heure,evenement) " \
+            "VALUES(%s,%s)"
+        args = (date, ev)
+
+        try:
+            cursor = self.mydb.cursor()
+            cursor.execute(query, args)
+            self.mydb.commit()
+        except Error as error:
+            print(error)
+
+ #-----------------------------------------------   
+
 
 if __name__ == '__main__':
     
     # valeur variables par defaut
-    limitFroid = 20.0
-    limitChaud = 80.0
+    limitFroid = 40.0
+    limitChaud = 50.0
     nbTick = 7
     token ="f0c51c904ed6dd637b2f"
     # si variable d'environnement existe, on le prend, sinon, valeur par defaut
@@ -99,10 +155,10 @@ if __name__ == '__main__':
     
     # test automatique avec les valeurs 20 et 80, pour les temperatures
     # if(testtype==0):
-    if "LIMITCHAUD" in os.environ:
-        limitChaud = float(os.environ["LIMITCHAUD"])
-    if "LIMITFROID" in os.environ:
-            limitFroid = float(os.environ["LIMITFROID"])
+    #if "LIMITCHAUD" in os.environ:
+        #limitChaud = float(os.environ["LIMITCHAUD"])
+    #if "LIMITFROID" in os.environ:
+           # limitFroid = float(os.environ["LIMITFROID"])
     # test dynamique. Les valeurs de température dependent de ce que l'utilisateur va entrer
     # elif(testtype==1):
     #     limitChaud= int(input("Entrer limite chaleur max de control : "))
