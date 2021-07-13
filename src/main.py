bd89161d22fc9ffe60b9d8c45414aef4faf8016a
moisecode <moise.mahara-moukene.2@etsmtl.net>
2021-07-12 23:30:49 -0400

    bug correction

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+
 from signalrcore.hub_connection_builder import HubConnectionBuilder
 import logging
 import sys
@@ -24,7 +25,8 @@ class Main:
     
         self.mydb = mysql.connect(
             user ='tp3',
-            password='Tp@3!55.',
+            password = os.environ["DB_PASS"],
+            #password='Tp@3!55.',
             host ='ec2-3-237-178-114.compute-1.amazonaws.com',
             database ='tp3'
         )
